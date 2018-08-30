8c03f13e5c2426a3ac9d8908749f7ff053cfeab3
sevenflash <sevenflash@yandex.ru>
2018-08-30 16:24:26 +0300

    qtum rpc

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { Facebook } from 'fb'
+import { Qtum } from 'qtumjs'
 
 import app from './app'
 
+const fb = new Facebook({ accessToken: process.env.ACCESS_TOKEN })
+
+const repoData = require('../solar.json')
+const qtum = new Qtum(process.env.RPC_URL, repoData)
+const users = qtum.contract('Users.sol')
+const achievements = qtum.contract('Achievements.sol')
+
 const port = process.env.PORT || 3000
-const accessToken = process.env.accessToken || ''
-
-const fb = new Facebook({ accessToken })
-
 app({
-  fb
+  fb, users, achievements
 }).listen(port, () => {
   console.log('Running on :${port}\n')
 })
 
 if (module.hot) {
-  module.hot.accpt('./app')
+  module.hot.accept('./app')
 }
