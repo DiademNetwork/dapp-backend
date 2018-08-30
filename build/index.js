9f89299a094c5f56c506d25fb4e8437bfc240b7f
sevenflash <sevenflash@yandex.ru>
2018-08-30 03:06:06 +0300

    build

---
+++ build/index.js
@@ -0,0 +1,114 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+
+// EXTERNAL MODULE: external "http"
+var external__http_ = __webpack_require__(2);
+var external__http__default = /*#__PURE__*/__webpack_require__.n(external__http_);
+
+// CONCATENATED MODULE: ./src/app.js
+const app = () => '<div style="padding: 20px"><h1>Welcome to dapp-backend</h1></div>';
+
+/* harmony default export */ var src_app = (app);
+// CONCATENATED MODULE: ./src/index.js
+
+
+
+const port = process.env.PORT || 3000;
+
+Object(external__http_["createServer"])((request, response) => response.end(src_app())).listen(port, () => process.stdout.write(`Running on :${port}\n`));
+
+Object(external__http_["createServer"])((request, response) => {
+  console.log(request);
+  response.end(src_app());
+}).listen(port, () => process.stdout.write(`Running on :${port}\n`));
+
+if (false) {
+  module.hot.accept('./app');
+}
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = require("http");
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 3000
 createServer((request, response) => response.end(app()))
   .listen(port, () => process.stdout.write(`Running on :${port}\n`))
 
+createServer((request, response) => {
+  console.log(request)
+  response.end(app())
+}).listen(port, () => process.stdout.write(`Running on :${port}\n`))
+
 if (module.hot) {
   module.hot.accept('./app')
 }
